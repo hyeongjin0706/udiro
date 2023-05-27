25326b3d7522c44ce37f21a728860a5fe39ce8cb
hyeongjin0706 <jja4985@naver.com>
2023-05-27 23:55:59 +0900

    router 따는중

--- client/router/culture/festa.js
+++ client/router/culture/festa.js
@@ -1,33 +1,23 @@
 import express from 'express';
-import * as festaController from '../../controller/culture/festa.js';
-// import { body } from 'express-validator';
-// import { validate } from "../middleware/validator.js";
-// import { isAuth } from "../middleware/auth.js";
+
 const router = express.Router();
-// const validateCulture = [
-//     body('culture_content')
-//         .trim()
-//         .isLength({min : 4})
-//         .withMessage('text는 최소 4글자 이상 입력하세요!'),
-//     validate
-// ];
 
-// GET
-router.get('/', festaController.getFestas);
+// // GET
+// router.get('/', festaController.getFestas);
 
-// GET
-router.get('/:place_NUM', festaController.getFesta);
+// // GET
+// router.get('/:place_NUM', festaController.getFesta);
 
-// 이름으로 찾는거 없어서 대충 넣어둠
-router.get('/:fac_name', festaController.getFesta);
+// // 이름으로 찾는거 없어서 대충 넣어둠
+// router.get('/:fac_name', festaController.getFesta);
 
-// POST
-router.post('/', festaController.CreateFesta);
+// // POST
+// router.post('/', festaController.CreateFesta);
 
-// PUT
-router.put('/:place_NUM', festaController.updateFesta);
+// // PUT
+// router.put('/:place_NUM', festaController.updateFesta);
 
-// DELETE
-router.delete('/:place_NUM', festaController.deleteFesta);
+// // DELETE
+// router.delete('/:place_NUM', festaController.deleteFesta);
 
 export default router;
--- client/router/culture/place.js
+++ client/router/culture/place.js
@@ -1,32 +1,20 @@
 import express from 'express';
-import * as placeController from '../../controller/culture/place.js';
-// import { body } from 'express-validator';
-// import { validate } from "../middleware/validator.js";
-// import { isAuth } from "../middleware/auth.js";
+
 const router = express.Router();
-// const validateCulture = [
-//     body('culture_content')
-//         .trim()
-//         .isLength({min : 4})
-//         .withMessage('text는 최소 4글자 이상 입력하세요!'),
-//     validate
-// ];
 
-// GET
-router.get('/', placeController.getPlaces);
 
-// GET
-router.get('/:place_NUM', placeController.getPlace);
+// // GET
+// router.get('/:place_NUM', placeController.getPlace);
 
-router.get('/:fac_name', placeController.getPlaceByFac_name);
+// router.get('/:fac_name', placeController.getPlaceByFac_name);
 
-// POST
-router.post('/', placeController.CreatePlace);
+// // POST
+// router.post('/', placeController.CreatePlace);
 
-// PUT
-router.put('/:place_NUM', placeController.UpdatePlace);
+// // PUT
+// router.put('/:place_NUM', placeController.UpdatePlace);
 
-// DELETE
-router.delete('/:place_NUM', placeController.deletePlace);
+// // DELETE
+// router.delete('/:place_NUM', placeController.deletePlace);
 
 export default router;
