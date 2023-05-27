f735f6f7d54dece6b2ef4e08b75c61aa1257417d
hyeongjin0706 <jja4985@naver.com>
2023-05-27 22:25:40 +0900

    index.ejs render 성공

--- client/index.js
+++ client/index.js
@@ -1,15 +1,17 @@
 import express from 'express';
+import ejs from "ejs";
 
 const app = express();
 
 app.set('view engine', 'ejs'); //view engine이 사용할 Template Engine
+app.set('views', "./public/ejs");
 app.use(express.static('public'));
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
 });
 
-
+// app.use("/culture", )
 
 app.use((req, res, next) => {
     res.sendStatus(404)
