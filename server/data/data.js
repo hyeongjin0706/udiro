20a650298120ad36096f1d0ec65b7814480f5f0f
Eadgnus <de_crystal@naver.com>
2023-05-31 21:00:13 +0900

    10분에 한번씩 데이터 업뎃

--- server/data/data.js
+++ server/data/data.js
@@ -10,7 +10,6 @@ export const Data = sequelize.define(
     {
         idx: {
             type: DataTypes.INTEGER,
-            allowNull: false,
             primaryKey: true,
             autoIncrement: true
         },
@@ -80,10 +79,11 @@ export const Data = sequelize.define(
         },
     },
     {
-        timestamps: false,
+        timestamps: true,
     }
 );
 // Data 모델과 연결된 데이터베이스 테이블이 존재하는지 확인하고 없다면 생성
+
 Data.sync({ force: false })
     .then(() => {
         console.log('Data 모델과 연결된 데이터베이스 테이블이 성공적으로 생성되었습니다.');
@@ -96,7 +96,6 @@ Data.sync({ force: false })
 const place = ['남산공원', '강남 MICE 관광특구', '동대문 관광특구', '명동 관광특구', '이태원 관광특구', '잠실 관광특구', '종로·청계 관광특구', '홍대 관광특구', '경복궁·서촌마을', '광화문·덕수궁', '창덕궁·종묘', '가산디지털단지역', '강남역', '건대입구역', '고속터미널역', '교대역', '구로디지털단지역', '서울역', '선릉역', '신도림역', '신림역', '신촌·이대역', '역삼역', '연신내역', '용산역', '왕십리역', 'DMC(디지털미디어시티)', '창동 신경제 중심지', '노량진', '낙산공원·이화마을', '북촌한옥마을', '가로수길', '성수카페거리', '수유리 먹자골목', '쌍문동 맛집거리', '압구정로데오거리', '여의도', '영등포 타임스퀘어', '인사동·익선동', '국립중앙박물관·용산가족공원', '뚝섬한강공원', '망원한강공원', '반포한강공원', '북서울꿈의숲', '서울대공원', '서울숲공원', '월드컵공원', '이촌한강공원', '잠실종합운동장', '잠실한강공원']
 
 export async function dataSave() {
-    console.log("실행중");
     try {
         await Promise.all(place.map(async (e) => {
             const url = `http://openapi.seoul.go.kr:8088/4d66634f6a776c7436315456716566/xml/citydata/1/5/${e}`;
@@ -135,7 +134,6 @@ export async function dataSave() {
                 const AREA_CONGEST_MSG = people[0].getElementsByTagName('AREA_CONGEST_MSG')[0].textContent
                 const NON_RESNT_PPLTN_RATE = people[0].getElementsByTagName('NON_RESNT_PPLTN_RATE')[0].textContent
 
-                console.log('데이터파싱완')
                 // Data 모델에 해당하는 데이터 생성하기 (sequelize의 create 메소드 사용)
                 await Data.update({
                     AREA_NM: AREA_NM,
@@ -154,7 +152,9 @@ export async function dataSave() {
                     ROAD_MSG: ROAD_MSG,
                     ROAD_TRAFFIC_SPD: ROAD_TRAFFIC_SPD,
                     NON_RESNT_PPLTN_RATE: NON_RESNT_PPLTN_RATE
-                }, { where: { idx: Data.idx } })
+                },
+                    { where: { AREA_NM: AREA_NM } }
+                )
                     .then((result) => {
                         console.log('데이터 업데이트 성공:', result);
                     })
@@ -173,10 +173,10 @@ export async function dataSave() {
 
 
 
-// setInterval(async () => {
-await dataSave();
-//     console.log('data업데이트 완료')
-// }, 600000);
+setInterval(async () => {
+    await dataSave();
+    console.log('data업데이트 완료')
+}, 600000);
 
 
 
